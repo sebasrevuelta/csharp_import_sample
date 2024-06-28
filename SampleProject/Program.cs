3b2da391087422adabfbb51c7f93ed6516eb0045
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2024-06-28 14:18:10 +0200

    Create Program.cs

---
+++ SampleProject/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using SampleLibrary;
+
+namespace SampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var sample = new SampleClass();
+            sample.DoSomething();
+        }
+    }
+}
