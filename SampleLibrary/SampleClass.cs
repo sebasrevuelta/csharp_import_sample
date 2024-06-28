7f344c294ecfc41cd69e4fbcc2dfc4e8a9aca501
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2024-06-28 14:18:42 +0200

    Create SampleClass.cs

---
+++ SampleLibrary/SampleClass.cs
@@ -0,0 +1,10 @@
+namespace SampleLibrary
+{
+    public class SampleClass
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing something...");
+        }
+    }
+}
